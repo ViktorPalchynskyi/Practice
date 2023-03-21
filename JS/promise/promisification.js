3317bf54896792158752d493b1a1d255cce8c5dd
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-03-21 21:30:51 +0300

    21.03.23 today: promisification

---
+++ JS/promise/promisification.js
@@ -0,0 +1,23 @@
+const promisify = (func, manyArgs = false) => function (...args) {
+   return new Promise((resolve, reject) => {
+      function callback(error, ...results) {
+         if (error) reject(error)
+         else resolve(manyArgs ? results : results[0]);
+      }
+
+      args.push(callback);
+      func.call(this, ...args);
+   });
+};
+
+function loadScript(src, callback) {
+   const script = document.createElement('script');
+   script.src = src;
+   script.onload = () => callback(null, script);
+   script.onerror = () => callback(new Error('Error during loading script'));
+   document.head.append(script);
+}
+
+const promisifyLoadScript = promisify(loadScript);
+promisifyLoadScript('./testFile.js')
+   .then(res => console.log('res', res.src))
