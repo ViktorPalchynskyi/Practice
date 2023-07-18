eafc5cc033eaaf174ca76fb14a3786db1a09887f
Viktor Palchynskyi <viktor.palchynskyi@fivewalls.com.ua>
2023-07-18 20:59:46 +0300

    18.07.23 month exercise: search

---
+++ JS/browser/document/search.js
@@ -0,0 +1,16 @@
+const table = document.querySelector('#age-table');
+const label = document.querySelectorAll('label');
+const td = document.querySelector('td');
+const form = document.querySelector('form[name="search"]');
+
+const firstInput = document.querySelector('input');
+const inputs = document.querySelectorAll('input');
+const lastInput = inputs[inputs.length - 1];
+
+console.log(table);
+console.log(label);
+console.log(td);
+console.log(form);
+console.log(firstInput);
+console.log(inputs);
+console.log(lastInput);
