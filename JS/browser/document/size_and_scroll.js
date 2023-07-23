e21883cf8bb84083670a4bb52840bb3a27a22811
Viktor Palchynskyi <viktor.palchynskyi@fivewalls.com.ua>
2023-07-23 15:30:48 +0300

    23.07.23 today practice: size and scroll

---
+++ JS/browser/document/size_and_scroll.js
@@ -0,0 +1,7 @@
+// распахнуть элемент на всю высоту
+// const test = document.querySelector('.test');
+// test.addEventListener('click', () => test.style.height = `${test.scrollHeight}px`);
+
+const test2 = document.querySelector('.test2');
+test2.addEventListener('click', () => test2.scrollTop = 200);
+
