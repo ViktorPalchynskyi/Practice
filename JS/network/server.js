2d6dd06fb73c172dc4c6d489d8029435144b6642
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-08-19 16:35:01 +0300

    19.08.23 today practice: fetch-crossorigin v2

--- JS/network/server.js
+++ JS/network/server.js
@@ -1,7 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+const jsonParser = express.json();
 
 const port = 3000;
+
+app.use(jsonParser);
+app.use(
+    cors((req, cb) => {
+        const allowedOrigins = ['http://127.0.0.1:5500'];
+        const corsOptions = {
+            credentials: true,
+        };
+
+        corsOptions.origin = allowedOrigins.includes(req.header('Origin'));
+
+        cb(null, corsOptions);
+    })
+);
 
 app.get('/', (req, res) => {
     console.log('Rout started');
