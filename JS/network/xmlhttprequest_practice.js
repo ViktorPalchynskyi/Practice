8dd1901bb28ceec0527142ba27928ed974577d41
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-08-21 23:34:51 +0300

    21.08.23 today practice: xmlhttprequest

--- JS/network/xmlhttprequest_practice.js
+++ JS/network/xmlhttprequest_practice.js
@@ -1,4 +1,31 @@
-let xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+console.log('open');
 
-xhr.open('POST', 'http://anywhere.com/request');
+const xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'http://localhost:3000');
+
+xhr.responseType = 'html';
+
+xhr.send();
+
+xhr.onload = () => {
+  if (xhr.status != 200) {
+    alert('Error');
+  } else {
+    console.log(xhr.getAllResponseHeaders());
+    alert(`Downloaded ${xhr.response.length}`);
+  }
+};
+
+
+xhr.onprogress = function (event) {
+  if (event.lengthComputable) {
+    alert(`Получено ${event.loaded} из ${event.total} байт`);
+  } else {
+    alert(`Получено ${event.loaded} байт`); // если в ответе нет заголовка Content-Length
+  }
+};
+
+xhr.onerror = function () {
+  alert('Запрос не удался');
+};
