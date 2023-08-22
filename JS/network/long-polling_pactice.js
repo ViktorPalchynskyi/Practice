c6e9a1f4c7a9b006e4b7d25873da235935f39364
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-08-22 22:58:49 +0300

    22.09.23 today practice: long polling

---
+++ JS/network/long-polling_pactice.js
@@ -0,0 +1,17 @@
+async function subscribe() {
+  let response = await fetch('/subscribe');
+
+  if (response.status == 502) {
+    await subscribe();
+  } else if (response.status != 200) {
+    showMessage(response.statusText);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    await subscribe();
+  } else {
+    let message = await response.text();
+    showMessage(message);
+    await subscribe();
+  }
+}
+
+subscribe();
