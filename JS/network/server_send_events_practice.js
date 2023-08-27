ce85bdfd03b7d43f8264411bea52d2b4ec72a4fb
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-08-27 23:30:02 +0300

    27.08.23 today practice: update

--- JS/network/server_send_events_practice.js
+++ JS/network/server_send_events_practice.js
@@ -1,8 +1,1 @@
-let eventSource = new EventSource("/events/subscribe");
-
-eventSource.onmessage = function(event) {
-  console.log("Новое сообщение", event.data);
-  // этот код выведет в консоль 3 сообщения, из потока данных выше
-};
-
-// или eventSource.addEventListener('message', ...)
+let source = new EventSource("https://another-site.com/events");
