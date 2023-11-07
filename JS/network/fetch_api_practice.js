257392d62364714d22e80a2f7d0ed2f3ed8a8e77
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-11-07 23:47:48 +0300

    07.11.23 month practice: fetch api

--- JS/network/fetch_api_practice.js
+++ JS/network/fetch_api_practice.js
@@ -1,9 +1,6 @@
 const localURL = 'http://localhost:3000/analytics';
 
-console.log('fetch API');
-
 window.onunload = function () {
-  console.log('unload');
   fetch('localURL', {
     method: 'POST',
     headers: {
