92973556424fef9b5b8f256423fd21d136ee1424
Viktor Palchynskyi <palchinskij19@gmail.com>
2023-11-26 11:38:10 +0300

    26.11.23 today exercise: numbers

---
+++ JS/js_types/number/number_exercise.js
@@ -0,0 +1,9 @@
+// const numOne = Number(prompt('num1', ''));
+// const numTwo = Number(prompt('num2', ''));
+// const sum = numOne + numTwo;
+
+// console.log(sum);
+
+const random = (min, max) =>  min + Math.random() * (max - min);
+
+console.log(random(2, 5));
