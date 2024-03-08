2e0236f418a79eb4f69e2e323793d4ee6cc2c8c3
Viktor Palchynskyi <palchinskij19@gmail.com>
2024-03-08 13:58:37 +0300

    08.03.24 today practice: practice with os Node.js js module

---
+++ NodeJS/practice/node.js_modules/os/os.js
@@ -0,0 +1,14 @@
+const os = require('node:os');
+
+// console.log(os.platform());
+// console.log(os.arch());
+// console.log(os.cpus());
+// console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.homedir());
+// console.log(os.tmpdir());
+// console.log(os.uptime());
+console.log(os.userInfo());
+// console.log(os.networkInterfaces());
+console.log(os.endianness());
+console.log(os.loadavg());
