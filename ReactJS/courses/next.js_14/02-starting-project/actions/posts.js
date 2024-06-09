e678f35e301d11d78ea609b9127ac83557d13c91
Viktor <palchinskij19@gmail.com>
2024-06-09 15:52:37 +0300

    09.06.24 today practice: added image handling

--- ReactJS/courses/next.js_14/02-starting-project/actions/posts.js
+++ ReactJS/courses/next.js_14/02-starting-project/actions/posts.js
@@ -1,5 +1,6 @@
 'use server';
 
+import { uploadImage } from '@/lib/cloudinary';
 import { storePost } from '@/lib/posts';
 import { redirect } from 'next/navigation';
 
@@ -25,8 +26,16 @@ export async function createPost(formData) {
         return { errors };
     }
 
+    let imageUrl;
+ 
+    try {
+        imageUrl = await uploadImage(image);
+    } catch (error) {
+        throw new Error('Image upload failed, post was not created. Please try again later');
+    }
+
     await storePost({
-        imageUrl: '',
+        imageUrl,
         title,
         content,
         userId: 1,
