88b7c08e616935594dd6c5f4f6a7a4c48aa05fd6
Viktor <palchinskij19@gmail.com>
2024-06-09 17:53:12 +0300

    09.06.24 today practice:  request and data cashing

--- ReactJS/courses/next.js_14/03-starting-project/app/messages/page.js
+++ ReactJS/courses/next.js_14/03-starting-project/app/messages/page.js
@@ -1,16 +1,14 @@
 import Messages from '@/components/messages';
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
-  const messages = await response.json();
+    const response = await fetch('http://localhost:8080/messages', {
+        cache: 'no-store',
+    });
+    const messages = await response.json();
 
-  if (!messages || messages.length === 0) {
-    return <p>No messages found</p>;
-  }
+    if (!messages || messages.length === 0) {
+        return <p>No messages found</p>;
+    }
 
-  return <Messages messages={messages} />;
+    return <Messages messages={messages} />;
 }
