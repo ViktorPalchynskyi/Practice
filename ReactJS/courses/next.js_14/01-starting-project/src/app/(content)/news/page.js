9276aced5abc6db847a4f56c4204346a6f746d88
Viktor <palchinskij19@gmail.com>
2024-06-09 12:56:16 +0300

    09.06.24 today practice: server-side data fetching

--- ReactJS/courses/next.js_14/01-starting-project/src/app/(content)/news/page.js
+++ ReactJS/courses/next.js_14/01-starting-project/src/app/(content)/news/page.js
@@ -1,42 +1,12 @@
-'use client';
-
-import { DUMMY_NEWS } from '@/dummy-news';
 import { NewsList } from '@/src/components/NewsList/NewsList';
-import { useEffect, useState } from 'react';
 
-export default function NewsDetailPage() {
-    const [error, setError] = useState();
-    const [isLoading, setIsLoading] = useState(false);
-    const [news, setNews] = useState();
+export default async function NewsDetailPage() {
+    const res = await fetch('http://localhost:8080/news');
+    const news = await res.json();
 
-    useEffect(() => {
-        async function fetchNews() {
-            setIsLoading(true);
-            const res = await fetch('http://localhost:8080/news');
-
-            if (!res.ok) {
-                console.log(res);
-                setError('Faild to fetch news.');
-                setIsLoading(false);
-            }
-
-            const news = await res.json();
-
-            setIsLoading(false);
-            setNews(news);
-        }
-
-        fetchNews();
-    }, []);
-
-    if (isLoading) {
-        return <p>Loading...</p>;
+    if (!res.ok) {
+        throw new Error('Failed to fetch news.');
     }
-
-    if (error) {
-        return <p>{error}</p>;
-    }
-
 
     return (
         <>
