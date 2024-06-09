ca6d6886496c66f2326362536f5c7ab726408f20
Viktor <palchinskij19@gmail.com>
2024-06-09 22:21:36 +0300

    09.06.24 today practice: auth

--- ReactJS/courses/next.js_14/04-starting-project/components/auth-form.js
+++ ReactJS/courses/next.js_14/04-starting-project/components/auth-form.js
@@ -1,27 +1,29 @@
+import { auth } from '@/actions/auth.actions';
 import Link from 'next/link';
 
-export default function AuthForm() {
-  return (
-    <form id="auth-form">
-      <div>
-        <img src="/images/auth-icon.jpg" alt="A lock icon" />
-      </div>
-      <p>
-        <label htmlFor="email">Email</label>
-        <input type="email" name="email" id="email" />
-      </p>
-      <p>
-        <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
-      </p>
-      <p>
-        <button type="submit">
-          Create Account
-        </button>
-      </p>
-      <p>
-        <Link href="/">Login with existing account.</Link>
-      </p>
-    </form>
-  );
+export default function AuthForm({ mode }) {
+    return (
+        <form id="auth-form" action={auth.bind(null, mode)}>
+            <div>
+                <img src="/images/auth-icon.jpg" alt="A lock icon" />
+            </div>
+            <p>
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" id="email" />
+            </p>
+            <p>
+                <label htmlFor="password">Password</label>
+                <input type="password" name="password" id="password" />
+            </p>
+            <p>
+                <button type="submit">
+                {mode === 'login' ? 'Login' : 'Create account'}
+                </button>
+            </p>
+            <p>
+                {mode === 'login' && <Link href="/?mode=signup">Create an account.</Link>}
+                {mode === 'signup' && <Link href="/?mode=login">Login with existing account.</Link>}
+            </p>
+        </form>
+    );
 }
