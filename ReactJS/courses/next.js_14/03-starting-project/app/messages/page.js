e2f74f851e5250c2c93064f32c82f2f0503a8dde
Viktor <palchinskij19@gmail.com>
2024-06-09 18:29:59 +0300

    09.06.24 today practice: on-demand data caching

--- ReactJS/courses/next.js_14/03-starting-project/app/messages/page.js
+++ ReactJS/courses/next.js_14/03-starting-project/app/messages/page.js
@@ -6,7 +6,7 @@ import { getMessages } from '@/lib/messages';
 // export const revalidate = 5;
 // export const dynamic = 'force-dynamic';
 
-export default function MessagesPage() {
+export default async function MessagesPage() {
     // unstable_noStore();
     // const response = await fetch('http://localhost:8080/messages', {
     //     next: {
@@ -15,7 +15,7 @@ export default function MessagesPage() {
     // });
     // const messages = await response.json();
 
-    const messages = getMessages();
+    const messages = await getMessages();
 
     if (!messages || messages.length === 0) {
         return <p>No messages found</p>;
