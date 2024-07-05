82ae1d24c4168477dc2624664ba440fd3d5f0389
Viktor <palchinskij19@gmail.com>
2024-07-05 21:31:15 +0300

    05.07.24 today practice: app router

---
+++ ReactJS/courses/next.js_14/app_router/pages/portfolio/[id].js
@@ -0,0 +1,13 @@
+import { useRouter } from 'next/router';
+
+export default function PortfoliProjectPage() {
+    const router = useRouter();
+
+    router
+
+    return (
+        <div>
+            <h1>PortfoliProjectPage</h1>
+        </div>
+    );
+}
