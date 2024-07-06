f2c4fa0d939d4ced62164ffc15af963486addd06
Viktor <palchinskij19@gmail.com>
2024-07-06 21:12:02 +0300

    06.07.24 today practice: nested dynamic routes & paths

--- ReactJS/courses/next.js_14/app_router/pages/clients/[id]/[clientprojectid].js
+++ ReactJS/courses/next.js_14/app_router/pages/clients/[id]/[clientprojectid].js
@@ -1,15 +1,13 @@
 import { useRouter } from 'next/router';
 
-export default function PortfoliProjectPage() {
+export default function SelectedClientProjectPage() {
     const router = useRouter();
 
-    console.log(router.pathname);
     console.log(router.query);
-    console.log(router);
 
     return (
         <div>
-            <h1>PortfoliProjectPage</h1>
+            <h1>The Project Page for a specific project for a selected client</h1>
         </div>
     );
 }
---
+++ ReactJS/courses/next.js_14/app_router/pages/clients/index.js
@@ -0,0 +1,7 @@
+export default function ClientPage() {
+    return (
+        <div>
+            <h1>The clients page</h1>
+        </div>
+    );
+}
