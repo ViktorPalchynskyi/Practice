c853ee605b0ceb690d7c3f0c2c07f0ad85bb01e3
Viktor <palchinskij19@gmail.com>
2024-07-08 19:30:11 +0300

    08.07.24 today practice: navigating programmatically

--- ReactJS/courses/next.js_14/app_router/pages/clients/index.js
+++ ReactJS/courses/next.js_14/app_router/pages/clients/index.js
@@ -1,20 +1,27 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function ClientPage() {
+    const router = useRouter();
+
+    function loadProjectHandler() {
+        // router.push('/clients/viktor/projecta');
+        // router.push({ pathname: '/clients/[id]', query: { id: 'max', clientprojectid: 'projectb' });
+        router.replace('/clients/viktor/projecta');
+    }
+
     return (
         <div>
             <h1>The clients page</h1>
             <ul>
                 <li>
-                    <Link href="/clients/viktor" >Viktor</Link>
+                    <Link href="/clients/viktor">Viktor</Link>
                 </li>
                 <li>
-                    <Link href="/clients/max" >max</Link>
-                </li>
-                <li>
-                    <Link href={{ pathname: '/clints/[id]', query: { id: 'max' } }} >max</Link>
+                    <Link href={{ pathname: '/clients/[id]', query: { id: 'max' } }}>max</Link>
                 </li>
             </ul>
+            <button onClick={loadProjectHandler}>Click me</button>
         </div>
     );
 }
