dce1e9ff6dcd3caac9829bf4d15a6dc37ad95085
Viktor <palchinskij19@gmail.com>
2024-07-08 19:13:07 +0300

    08.07.24 today practice: link navigation

--- ReactJS/courses/next.js_14/app_router/pages/clients/index.js
+++ ReactJS/courses/next.js_14/app_router/pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from 'next/link';
+
 export default function ClientPage() {
     return (
         <div>
             <h1>The clients page</h1>
+            <ul>
+                <li>
+                    <Link href="/clients/viktor" >Viktor</Link>
+                </li>
+                <li>
+                    <Link href="/clients/max" >max</Link>
+                </li>
+                <li>
+                    <Link href={{ pathname: '/clints/[id]', query: { id: 'max' } }} >max</Link>
+                </li>
+            </ul>
         </div>
     );
 }
--- ReactJS/courses/next.js_14/app_router/pages/index.js
+++ ReactJS/courses/next.js_14/app_router/pages/index.js
@@ -1,7 +1,17 @@
+import Link from 'next/link';
+
 export default function HomePage() {
     return (
         <div>
             <h1>Hello there</h1>
+            <ul>
+                <li>
+                    <Link replace href="/clients">clients</Link>
+                </li>
+                <li>
+                    <Link replace href="/portfolio">portfolio</Link>
+                </li>
+            </ul>
         </div>
     );
 }
