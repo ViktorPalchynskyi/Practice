b4c4e478c7d02ac6f558432d605933dd96302bc9
Viktor <palchinskij19@gmail.com>
2024-07-08 20:45:26 +0300

    08.07.24 today practice: using getStaticProps

--- ReactJS/courses/next.js_14/app_router/pages/index.js
+++ ReactJS/courses/next.js_14/app_router/pages/index.js
@@ -1,17 +1,26 @@
-import Link from 'next/link';
+import { promises as fs } from 'fs';
+import path from 'path';
 
-export default function HomePage() {
+export default function HomePage({ products }) {
+    console.log('prod', products);
     return (
         <div>
             <h1>Hello there</h1>
             <ul>
-                <li>
-                    <Link replace href="/clients">clients</Link>
-                </li>
-                <li>
-                    <Link replace href="/portfolio">portfolio</Link>
-                </li>
+                {products.map((product) => (
+                    <li key={product.id}>{product.title}</li>
+                ))}
             </ul>
         </div>
     );
+}
+
+export async function getStaticProps() {
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonFile = await fs.readFile(filePath);
+    const data = JSON.parse(jsonFile);
+
+    console.log(data);
+
+    return { props: { products: data.products } };
 }
