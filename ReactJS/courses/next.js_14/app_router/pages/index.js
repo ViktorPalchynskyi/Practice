76b3fdce36c9c6044c09dbfd7cb98063e044d4b7
Viktor <palchinskij19@gmail.com>
2024-07-16 19:37:03 +0300

    16.07.24 today practice: getStaticProps options

--- ReactJS/courses/next.js_14/app_router/pages/index.js
+++ ReactJS/courses/next.js_14/app_router/pages/index.js
@@ -1,4 +1,5 @@
 import { promises as fs } from 'fs';
+import { redirect } from 'next/dist/server/api-utils';
 import path from 'path';
 
 export default function HomePage({ products }) {
@@ -14,11 +15,27 @@ export default function HomePage({ products }) {
     );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
     console.log('Re...');
     const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
     const jsonFile = await fs.readFile(filePath);
     const data = JSON.parse(jsonFile);
 
-    return { props: { products: data.products }, revalidate: 10 };
+    if (!data) {
+        return {
+            redirect: {
+                destination: '/no-data'
+            }
+        }
+    }
+
+    if (!data.products.length) {
+        return { notFound: true };
+    }
+
+    return {
+        props: { products: data.products },
+        revalidate: 10,
+        redirect: 
+    };
 }
