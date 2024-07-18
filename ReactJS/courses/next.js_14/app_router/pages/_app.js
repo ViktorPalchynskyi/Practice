9395779b7d5dc9da50317f56f08311cd286685a6
Viktor <palchinskij19@gmail.com>
2024-07-18 16:33:38 +0300

    18.07.24 today practice: added _document.js

--- ReactJS/courses/next.js_14/app_router/pages/_app.js
+++ ReactJS/courses/next.js_14/app_router/pages/_app.js
@@ -5,6 +5,7 @@ export default function App({ Component, pageProps }) {
     return (
         <>
             <Head>
+                <title>Main title can be overwritten</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
             <Component {...pageProps} />
